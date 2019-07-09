2e1bc515cba9de6398f9876fe6d17b26c38ce23b
jonatasilva <jonatas.iw@gmail.com>
2019-07-09 17:08:30 -0300

    deploy app to heroku

--- web.js
+++ web.js
@@ -17,7 +17,7 @@ app.use(express.static('./dist/{{thebrindesfrontend}}'));
 app.get('/*', function(req,res) {
     
 app.use(express.static('./dist/{{thebrindesfrontend}}'));
-res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/{{thebrindesfrontend}}/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
