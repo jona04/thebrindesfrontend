9243ffd8c92f08d649363d366af4368090b01d4f
jonatasilva <jonatas.iw@gmail.com>
2019-07-09 21:04:09 -0300

    deploy app to heroku

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.static(__dirname + '/src')); //aqui você define onde está o index.html da sua aplicação.
 app.listen(process.env.PORT || 3000);
 
-
+console.log(__dirname + '/src');
+console.log('./dist/{{thebrindesfrontend}}');
 
 // //Install express server
 // const express = require('express');
