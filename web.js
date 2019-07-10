d36d608dbda0131f18481be79fac37d479d40c0a
jonatasilva <jonatas.iw@gmail.com>
2019-07-09 21:16:21 -0300

    deploy app to heroku

--- web.js
+++ web.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/src')); //aqui você define onde está o index.html da sua aplicação.
-app.listen(process.env.PORT || 3000);
+// var express = require('express');
+// var app = express();
+// app.use(express.static(__dirname + '/src')); //aqui você define onde está o index.html da sua aplicação.
+// app.listen(process.env.PORT || 3000);
 
-console.log(__dirname + '/src');
-console.log('./dist/{{thebrindesfrontend}}');
+// console.log(__dirname + '/src');
+// console.log('./dist/{{thebrindesfrontend}}');
 
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/{{thebrindesfrontend}}'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// app.use(express.static('./dist/{{thebrindesfrontend}}'));
-// res.sendFile(path.join(__dirname,'/dist/{{thebrindesfrontend}}/index.html'));
-// });
+app.use(express.static('./dist'));
+res.sendFile(path.join(__dirname,'/dist/index.html'));
+});
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
